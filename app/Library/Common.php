b666113ad653504852fea9c68f7148be161f04d1
AshitamaH <super_ashumar@yahoo.co.jp>
2022-12-09 13:13:22 +0900

    Revert "test"
    This reverts commit 6c1e1f757fd628afd7ac42db72bb6aefa717c97a.

--- app/Library/Common.php
+++ app/Library/Common.php
@@ -27,12 +27,6 @@ class Common
         return $url;
     }
 
-    /**
-     * Undocumented function
-     *
-     * @param [type] $url
-     * @return void
-     */
     public static function callApi($url)
     {
         $json = file_get_contents($url);
